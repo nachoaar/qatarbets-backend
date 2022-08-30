ee6699c8412109bf59b2df76fb1fc42dda49b93e
gastonfuentes <gastonnicolasfuentes@gmail.com>
2022-08-30 20:33:07 -0300

    se agrego el sync de sequelize

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const server = require("./src/App.js");
 const port = process.env.BACK_HOST;
+const { conn } = require("./src/db.js");
 
 conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
