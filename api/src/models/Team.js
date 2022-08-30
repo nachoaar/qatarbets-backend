d5c1170e9595bec1a51bd356fcc197975e4fcd2a
gastonfuentes <gastonnicolasfuentes@gmail.com>
2022-08-30 18:20:28 -0300

    se creo modelo Team

---
+++ api/src/models/Team.js
@@ -0,0 +1,36 @@
+const { DataTypes, INTEGER } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "Team",
+    {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      logo: {
+        type: DataTypes.STRING,
+      },
+      coach: {
+        type: DataTypes.STRING,
+      },
+      code: {
+        type: DataTypes.STRING,
+      },
+      founded: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
