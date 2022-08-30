336a02ec969cf3dc6d19e97fc7732df36ce73468
faramir07 <97300150+faramir07@users.noreply.github.com>
2022-08-30 15:43:22 -0500

    refactor(api/src/db.js): change name DB

--- api/src/db.js
+++ api/src/db.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/qatarbet`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
