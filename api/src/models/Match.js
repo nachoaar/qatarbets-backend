5c82bf2c1cbe3e8570df574927dd42c55b055dc4
JorgeCanale <jorgecanale636@yahoo.com>
2022-08-30 18:56:18 -0300

    cree el modelo de match en la carpeta models

---
+++ api/src/models/Match.js
@@ -0,0 +1,43 @@
+const {DataTypes} = require('sequelize')
+
+module.exports  = (sequelize) =>{
+    sequelize.define('match',{
+        id:{
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+        },
+        date:{
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        status:{
+            type: DataTypes.STRING
+        },
+        home_team_id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        away_team_id:{
+            type: DataTypes.INTEGER,
+            allowNull:false,
+        },
+        result_match:{
+            type: DataTypes.ENUM,
+            allowNull:false
+        },
+        stadium_name:{
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        city:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        group_id:{
+            type: DataTypes.INTEGER
+        }
+
+
+    })
+}
