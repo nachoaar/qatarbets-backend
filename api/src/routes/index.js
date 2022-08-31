f29c30f5ba238f3ab204ce3e7ed19b5380a097a5
matiasmanuelmontes <matiasmanuelmontes@gmail.com>
2022-08-31 20:57:58 -0300

    Routes get hisBet params & get hisBet, still working - Matias Montes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,9 +4,11 @@ const router = Router();
 
 const AllTeams = require('./worldCupTeams/worldCupTeams.js');
 const fixtureRoute = require('./wcFixtures/wcFixtures.js');
+const hisBetsRoute = require('./hisBets/hisBets.js');
 
 router.use('/Teams&Squads', AllTeams);
 router.use('/fixture', fixtureRoute);
+router.use('/hisBets', hisBetsRoute);
 
 
 module.exports = router;
