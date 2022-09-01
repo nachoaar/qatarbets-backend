bff84e92062b75c413066904bb35afe190bb33d1
faramir07 <97300150+faramir07@users.noreply.github.com>
2022-09-01 13:18:09 -0500

    feat(api/src/routes/index.js) add route group

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
+const express = require('express');
 const router = Router();
-
 const fixtureRoute = require('./wcFixtures/wcFixtures.js');
+const groups = require('./groups/groups.js')
+
+router.use(express.json());
+
 
 router.use('/fixture', fixtureRoute);
+router.use('/groups', groups)
 
 module.exports = router;
