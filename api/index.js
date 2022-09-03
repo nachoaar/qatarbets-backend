5e182e6c273f0b59ce9c3c799537f25ebb9b3017
matuxer <matias@biott.com.ar>
2022-09-03 09:18:20 -0300

    Added db sync

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const server = require("./src/App.js");
 const PORT = 3001;
-//const { conn } = require("./src/db.js");
+const { conn } = require("./src/db.js");
 
-server.listen(process.env.PORT || PORT, () => {
-  console.log(`Servidor Activo!`);
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT || PORT, () => {
+    console.log(`Servidor Activo!`);
+  });
 });
