3a4f97cb0853e6e86ae1496060741ff9e2ecae22
matuxer <matias@biott.com.ar>
2022-09-03 09:24:40 -0300

    Testing deploy with index.js

--- api/index.js
+++ api/index.js
@@ -3,8 +3,12 @@ const server = require("./src/App.js");
 const PORT = 3001;
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: true }).then(() => {
+/* conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT || PORT, () => {
     console.log(`Servidor Activo!`);
   });
+}); */
+
+server.listen(process.env.PORT || PORT, () => {
+  console.log(`Servidor Activo!`);
 });
