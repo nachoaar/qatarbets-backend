69ed2f30dd78a3e56317132e884ee1408c646c2e
matuxer <matias@biott.com.ar>
2022-09-02 21:31:21 -0300

    Cambie db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 const { match } = require("assert");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 /* const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/qatarbets`,
