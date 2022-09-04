b479e04f72bea89597b0af2fdc37589b5760fc47
unknown <facu_789_123@hotmail.com>
2022-09-04 15:30:49 -0300

    Conflictos resueltos

---
+++ api/src/DB_variables.js
@@ -0,0 +1,12 @@
+
+const DB_USER="postgres";
+const DB_PASSWORD="1234";
+const DB_HOST="localhost";
+const API_KEY="993943c6a7d4a29527f6e9c92b7d0541";
+
+module.exports = {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  API_KEY
+}
