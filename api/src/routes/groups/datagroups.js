1552389291e7b8e3742a9145dab8bedba560e1d9
matuxer <matias@biott.com.ar>
2022-09-04 17:28:47 -0300

    Arreglado orden de id de los grupos

--- api/src/routes/groups/datagroups.js
+++ api/src/routes/groups/datagroups.js
@@ -8,18 +8,32 @@ async function dataGroups() {
   })
 
   if(getGroupsDB.length === 0){
-    const data = ['grupo_A', 'grupo_B', 'grupo_C', 'grupo_D', 'grupo_E', 'grupo_F', 'grupo_G', 'grupo_H']
+    //const data = ['grupo_A', 'grupo_B', 'grupo_C', 'grupo_D', 'grupo_E', 'grupo_F', 'grupo_G', 'grupo_H']
+    const data = [
+      { id: 1, name: 'grupo_A' },
+      { id: 2, name: 'grupo_B' },
+      { id: 3, name: 'grupo_C' },
+      { id: 4, name: 'grupo_D' },
+      { id: 5, name: 'grupo_E' },
+      { id: 6, name: 'grupo_F' },
+      { id: 7, name: 'grupo_G' },
+      { id: 8, name: 'grupo_H' },
+    ]
 
   const createDbGroup =  data.forEach( async (group) => {
     await Group.findOrCreate({
     where: {
-      name: group
+      id: group.id,
+      name: group.name
     }
   })
 });
   }
 
   const getGroupsTeamDB = await Group.findAll({
+    order : [
+      ['id', 'ASC']
+    ],
     include: {
       model: Team,
       attributes: ['name', 'logo']
