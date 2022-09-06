0abc3529b1a60101797c661df737421650bfb91d
matuxer <matias@biott.com.ar>
2022-09-06 17:37:33 -0300

    changed id type in user model

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -5,12 +5,6 @@ module.exports = (sequelize) => {
   sequelize.define(
     'user',
     {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true
-      },
       name: {
         type: DataTypes.STRING,
         allowNull: false,
