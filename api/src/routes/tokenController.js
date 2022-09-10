249a1c1632b1ed668b39b4e75ed138845f29d03f
matuxer <matias@biott.com.ar>
2022-09-10 18:27:40 -0300

    Added file to create and validate tokens

---
+++ api/src/routes/tokenController.js
@@ -0,0 +1,30 @@
+const { sign, verify } = require("jsonwebtoken");
+
+const createTokens = (user) => {
+  const accessToken = sign({
+    name: user.name,
+    id: user.id,
+  }, "jwtsecretplschange");
+
+  return accessToken;
+};
+
+const validateToken = (accessToken) => {
+  
+  try {
+    const validToken = verify(accessToken, "jwtsecretplschange");
+    if (validToken) {
+      return validToken;
+    } else {
+      return '';
+    }
+  } catch (error) {
+    console.log(error);
+    return '';
+  }
+}
+
+module.exports = {
+  createTokens,
+  validateToken,
+};
