d8f54513be8b10c69177524fdbb3d0017ee33093
matuxer <matias@biott.com.ar>
2022-09-10 18:23:21 -0300

    added credential to true in cors

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const { conn } = require("./src/db.js");
 const cors = require('cors');
 
 conn.sync({ force: false }).then(() => {
-  server.use(cors({ origin: '*' }));
+  server.use(cors({ credentials: true }));
   server.listen(process.env.PORT || PORT, () => {
     console.log(`Servidor Activo!`);
   });
