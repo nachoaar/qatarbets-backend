061392c5acab4cd11fa46c7db6ac53a9ed912f08
matiasmanuelmontes <matiasmanuelmontes@gmail.com>
2022-09-09 22:51:52 -0300

    agrego ruta para obtener el profit una vez finalizado el partido

--- api/src/DB_variables.js
+++ api/src/DB_variables.js
@@ -1,15 +1,14 @@
 
 //Variables Mati B
 
-  const DB_USER="postgres";
+   const DB_USER="postgres";
 const DB_PASSWORD=2002;
 const DB_HOST="localhost";
-const API_KEY="24a8ce846af0f2ea149d31a7d48f829b";      
+const API_KEY="24a8ce846af0f2ea149d31a7d48f829b";       
 
  
 //variables MatiasM
-
-/*     const DB_USER="postgres";
+/*   const DB_USER="postgres";
 const DB_PASSWORD="12345m4";
 const DB_HOST="localhost";
 const API_KEY="993943c6a7d4a29527f6e9c92b7d0541";   */ 
