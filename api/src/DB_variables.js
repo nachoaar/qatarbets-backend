5ab531895818373c98a63424dfe25f9765081597
matuxer <matias@biott.com.ar>
2022-09-11 18:05:42 -0300

    modified gitignore

---
+++ api/src/DB_variables.js
@@ -0,0 +1,15 @@
+/* 
+git rm -rf --cached .
+git add .
+*/
+const DB_USER="postgres";
+const DB_PASSWORD=2002;
+const DB_HOST="localhost";
+const API_KEY="24a8ce846af0f2ea149d31a7d48f829b";
+
+module.exports = {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  API_KEY
+}
