dd89b5f930938d1bc947f6f3695dbc4c6d0eec4a
matiasmanuelmontes <matiasmanuelmontes@gmail.com>
2022-09-11 23:41:33 -0300

    pruebo un cambio en las url de deploy del back

--- api/src/App.js
+++ api/src/App.js
@@ -13,12 +13,31 @@ server.use(express.json() );
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${URL}`);
+  /*  res.header('Access-Control-Allow-Origin', `${URL}`);  */
+  const allowedOrigins = [ process.env.DEPLOY_URL, 'https://qatarbets-frontend-git-develop-nachoaar.vercel.app', 'http://localhost:3000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  } 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+
+// evaluar el siguiente codigo
+/* app.use((req, res, next) => {
+  const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+}); */
 
 // variable de session
 const session = require('express-session');
