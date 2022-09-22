a8ee74b963418df1d9e5f1c7063076ab1ade27b3
unknown <facu_789_123@hotmail.com>
2022-09-21 21:18:51 -0300

    corregido email de registro

--- api/src/routes/user/user.js
+++ api/src/routes/user/user.js
@@ -226,9 +226,8 @@ router.post("/login", async (req, res, next) => {
               <td style="text-align: center; padding: 15px;">
               <p style="font-size: 20px; font-weight: bold;">Te has logueado correctamente!!</p>
               <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">Hemos registrado que te has logueado recientemente en tu cuenta de QATARBETS.</p>
-              <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">En caso de que no hayas sido tú, puedes cambia tu contraseña tocando el siguiente botón.</p>
+              <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">En caso de que no hayas sido tú, puedes ponerte en contacto con nosotros.</p>
               <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">En caso de que sí hayas sido tú, puedes ignorar este correo.</p>
-              <a href="https://qatarbets-frontend-git-develop-nachoaar.vercel.app/userForgottenPass" class="boton">Cambia tu contraseña</a>
               </td>
             </tr>
           </table>
@@ -465,7 +464,7 @@ router.post("/login", async (req, res, next) => {
 //   }
 // });
 
-router.post("/login/mobile", async (req, res) => {
+router.post("/login/mobile", async (req, res) => {git 
   var { pass, email } = req.body;
   email = email.toLowerCase();
   try {
@@ -511,157 +510,6 @@ router.post("/login/mobile", async (req, res) => {
           });
         }
       }
-    });
-    await transporter.sendMail({
-      from: '"QatarBets" <QatarBets2022@gmail.com>', //Emisor
-      to: email, //Receptor
-      subject: "QATARBETS login", //Asunto
-      html: `<!DOCTYPE html>
-      <html lang="eng">
-      <head>
-      <meta charset="utf-8"/>
-
-      <!-- ESTILOS -->
-      <style type="text/css">
-        body {
-          margin: 0;
-          background-color: #ececec;
-        }
-        table {
-          border-spacing: 0;
-        }
-        td {
-          padding: 0;
-        }
-        img {
-          border: 0;
-        }
-        .wrapper {
-          width: 100%;
-          table-layout: fixed;
-          background-color: #ececec;
-          padding-bottom: 5px;
-        }
-        .main {
-          background-color: #000000;
-          margin: 0 auto;
-          width: 100%;
-          max-width: 600px;
-          border-spacing: 0; 
-          font-family: sans-serif;
-          color: #171a1b;
-        }
-        .dos-columnas{
-          text-align: center;
-          font-size: 0;
-        }
-        .dos-columnas .columna{
-          width: 100%;
-          max-width: 300px;
-          display: inline-block;
-          vertical-align: top;  
-          text-align: center;
-        }
-        .boton{
-          background-color: red;
-          color: #ffffff;
-          text-decoration: none;
-          padding: 12px 20px;
-          border-radius: 5px;
-          font-weight: bold;
-        }
-      </style>   
-      </head>
-      <body>
-      <center class="wrapper">
-      <table class="main" width="100%">
-
-      <!-- BORDE SUPERIOR -->
-      <tr>
-         <td height="8" style="background-color: red;"></td>
-      </tr>
-
-      <!-- SECCION DE LOGOS -->
-      <tr>
-         <td style="padding: 14px 0 4px; background-color: rgb(96, 2, 96);">   
-             <table width="100%">
-
-                <tr>
-                   <td class="dos-columnas">
-                   
-                       <table class="columna">
-                          <tr>
-                             <td style="padding: 0 62px 10px;">
-                               <a href="https://qatarbets-frontend-git-develop-nachoaar.vercel.app"><img src="https://i.imgur.com/cf7jqcN.png" width="250" alt="Encabezado" title="Encabezado logo"/></a>
-                             </td>
-                          </tr>   
-                       </table>
-
-                       <table class="columna">
-                        <tr>
-                           <td style="padding: 40px 86px;">
-                             <a style="margin-left: 5px;" href="https://www.facebook.com/QatarBetsLATAM"><img src="https://i.imgur.com/G3cmjre.png" width="27" alt="facebook" /></a>
-                             <a style="margin-left: 5px;" href="https://twitter.com/QatarBets"><img src="https://i.imgur.com/x8Fq8j4.png" width="27" alt="twitter"/></a>
-                             <a style="margin-left: 5px;" href="https://www.instagram.com/qatar_bets/"><img src="https://i.imgur.com/31bbcEa.png" width="27" alt="instagram"/></a>
-                             <a style="margin-left: 5px;" href="https://www.linkedin.com/company/qatar-bets/"><img src="https://i.imgur.com/st6Gh4d.png" width="27" alt="linkedin"/></a>
-                           </td>
-                        </tr>   
-                     </table>
-
-                   </td>
-                </tr>
-
-             </table>
-         </td>
-      </tr>
-      <!-- MENSAJE -->
-      <tr>
-        <td style="padding: 15px 0 50px; background-color: #ffffff;"> 
-          <table style="background-color:#ffffff; width: 100%;">
-            <tr>
-              <td style="text-align: center; padding: 15px;">
-              <p style="font-size: 20px; font-weight: bold;">Te has logueado correctamente!!</p>
-              <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">Hemos registrado que te has logueado recientemente en tu cuenta de QATARBETS.</p>
-              <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">En caso de que no hayas sido tú, puedes cambia tu contraseña tocando el siguiente botón.</p>
-              <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">En caso de que sí hayas sido tú, puedes ignorar este correo.</p>
-              <a href="https://qatarbets-frontend-git-develop-nachoaar.vercel.app/userForgottenPass" class="boton">Cambia tu contraseña</a>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-      <!-- FOOTER -->
-      <tr>
-        <td style="background-color: rgb(96, 2, 96);">
-          <table>
-
-            <tr>
-              <td style="text-align: center; padding: 45px 20px; color: #ffffff;">
-                <a href="https://qatarbets-frontend-git-develop-nachoaar.vercel.app">
-                  <img src="https://i.imgur.com/oudfmJO.png" alt="logo" width="50"/> <a style="margin-left: 60px; color: #ffffff;">Todos los derechos reservados © 2022 QATARBETS</a>
-                </a>
-              </td>
-            </tr>
-
-          </table>
-        </td>
-      </tr>
-      <tr>
-        <td style="background-color: #ececec">
-          <table>
-            <tr>
-              <td style="text-align: left; margin-top: 10px;">
-                <p style="margin-top: 10px; color: #8d8d8d; font-size: 13px;">QATARBETS, Inc.</p>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-
-      </table>
-      </center>
-      </body>
-      </html>`,
     });
   } catch (error) {
     res.json("a" + error);
@@ -825,7 +673,7 @@ router.post("/register", async (req, res, next) => {
               <p style="font-size: 20px; font-weight: bold;">Bienvenido ${name} a QATARBETS!!</p>
               <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">Ya se ha registrado correctamente en QATARBETS, solo le queda cumplir un último paso para completar su registro.</p>
               <p style="line-height: 23px; font-size: 15px; padding: 5px 0 15px;">Para que pueda acceder a todos nuestros servicios es necesario que verifique su cuenta:</p>
-              <a href="https://qatarbets-backend-production-ab54.up.railway.app/verify/${EmailVerify}" class="boton">Verifique su cuenta aquí</a>
+              <a href="https://qatarbets-backend-production.up.railway.app/userVerify/${EmailVerify}" class="boton">Verifique su cuenta aquí</a>
               </td>
             </tr>
           </table>
